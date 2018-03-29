1f9b476c1eae14c51a5f5855ba87dee0818227c3
Pattrick Hueper <phueper@hueper.net>
2018-03-29 21:20:59 +0200

    RX-12898: remove window.rplanConfig dependency
    - change init logic, no more auto init, users of this module need to init with google Tracking ID
    - add debug logging
    - get rid of `isValidDomain` when initialized with a trackingId, the module will now try to track, no more domain checking
      so the user of the module is responsible wether tracking will be done or not

--- out/client.js
+++ out/client.js
@@ -4,33 +4,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _get2 = require('lodash/get');
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var _get3 = _interopRequireDefault(_get2);
+var _debug2 = require('debug');
+
+var _debug3 = _interopRequireDefault(_debug2);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// TODO: more or less duplicated from rplan/lib/config-client,
-// extract into a @rplan/config-client module?
-var runtimeConfig = function runtimeConfig() {
-  if (typeof window !== 'undefined' && window.rplanConfig) {
-    return window.rplanConfig;
-  }
-  return {};
-};
-var config = {
-  get: function get(path) {
-    return (0, _get3.default)(runtimeConfig(), path);
-  }
-};
+var debug = (0, _debug3.default)('user-tracking');
 
 var UserTracking = function UserTracking() {
-  var isValidDomain = function isValidDomain() {
-    return typeof window !== 'undefined' && window.location.hostname === config.get('domain');
+  var trackingId = void 0;
+
+  var isInitialized = function isInitialized() {
+    return trackingId && typeof window.ga === 'function';
   };
 
-  var init = function init() {
-    if (isValidDomain()) {
+  var load = function load() {
+    if (typeof window.ga !== 'function') {
+      debug('load analytics.js');
       /* for the init function copied from google analytics */
       /* eslint-disable no-param-reassign */
       (function (i, s, o, g, r, a, m) {
@@ -44,19 +37,28 @@ var UserTracking = function UserTracking() {
         };
         i[r].l = 1 * new Date();
         a = s.createElement(o);
-        m = s.getElementsByTagName(o)[0];
+
+        var _s$getElementsByTagNa = s.getElementsByTagName(o);
+
+        var _s$getElementsByTagNa2 = _slicedToArray(_s$getElementsByTagNa, 1);
+
+        m = _s$getElementsByTagNa2[0];
+
         a.async = 1;
         a.src = g;
         m.parentNode.insertBefore(a, m);
       })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+      /* eslint-enable no-param-reassign */
     }
   };
 
-  var create = function create() {
-    var trackingId = config.get('googleTrackingId');
-    if (isValidDomain() && trackingId) {
-      window.ga('create', trackingId, 'auto');
+  var init = function init(_trackingId) {
+    if (_trackingId) {
+      debug('init with trackingId: ', _trackingId);
+      load();
+      window.ga('create', _trackingId, 'auto');
       window.ga('set', 'anonymizeIp', true);
+      trackingId = _trackingId;
     }
   };
 
@@ -65,13 +67,15 @@ var UserTracking = function UserTracking() {
    * https://developers.google.com/analytics/devguides/collection/analyticsjs/events
    */
   var sendEvent = function sendEvent(category, action, label, value) {
-    if (isValidDomain()) {
+    if (isInitialized()) {
+      debug('sendEvent', category, action, label, value);
       window.ga('send', 'event', category, action, label, value);
     }
   };
 
   var traceButtonClick = function traceButtonClick(element, eventLabel, eventValue) {
-    if (element && isValidDomain()) {
+    if (element && isInitialized()) {
+      debug('traceButtonClick', element, eventLabel, eventValue);
       element.addEventListener('click', function () {
         return sendEvent('button', 'click', eventLabel, eventValue);
       });
@@ -79,18 +83,14 @@ var UserTracking = function UserTracking() {
   };
 
   var send = function send(optFieldObject) {
-    if (isValidDomain()) {
+    if (isInitialized()) {
+      debug('send', optFieldObject);
       window.ga('send', optFieldObject);
     }
   };
 
-  init();
-  create();
-
   return {
     init: init,
-    isValidDomain: isValidDomain,
-    create: create,
     traceButtonClick: traceButtonClick,
     send: send,
     sendEvent: sendEvent
@@ -133,8 +133,6 @@ var ConversionTracking = function ConversionTracking(customElement) {
       cb();
     }
   };
-
-  init();
 
   return {
     init: init,
