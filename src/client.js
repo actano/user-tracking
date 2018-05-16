24a1324f0632936f9ac74138303500f9d4714de5
Pattrick Hueper <phueper@hueper.net>
2018-05-16 18:26:18 +0200

    fix user-tracking for IE11

--- src/client.js
+++ src/client.js
@@ -18,8 +18,10 @@ const UserTracking = () => {
           (i[r].q = i[r].q || []).push(args)
         }
         i[r].l = 1 * new Date()
-        a = s.createElement(o);
-        [m] = s.getElementsByTagName(o)
+        a = s.createElement(o)
+        // don't use destructuring syntax, it seems to break IE11 :/
+        /* eslint-disable-next-line prefer-destructuring */
+        m = s.getElementsByTagName(o)[0]
         a.async = 1
         a.src = g
         m.parentNode.insertBefore(a, m)
